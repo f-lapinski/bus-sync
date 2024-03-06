c7350f3f646e7ae533b4d9b8a73f5b8c7c85a486
f-lapinski <filip.lapinski@proton.me>
2024-03-06 12:03:59 +0100

    Add Vehicle model and register it in admin panel

--- src/vehicles/models.py
+++ src/vehicles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    inv_number = models.CharField(max_length=100)
+    register_number = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
