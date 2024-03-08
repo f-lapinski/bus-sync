9284eb0fbf8da9981bc22a5dfec6f2618b7c982f
f-lapinski <filip.lapinski@proton.me>
2024-03-08 15:39:48 +0100

    Add endpoint for view detail of vehicle

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -6,3 +6,7 @@ def index(request):
     vehicles = Vehicle.objects.all()
     for vehicle in vehicles:
         return HttpResponse(str(vehicle))
+
+
+def detail(request, vehicle_id):
+    return HttpResponse(Vehicle.objects.get(id=vehicle_id))
