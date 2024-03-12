1cfee7ab01ca1b994de18120ffe0e3482a2126ee
f-lapinski <filip.lapinski@proton.me>
2024-03-12 14:48:29 +0100

    Add templates for all vehicles and vehicle details

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from .models import Vehicle
 
 
 def index(request):
-    return None
+    vehicles = Vehicle.objects.all()
+    return render(request, 'vehicles/all_vehicles.html', {'vehicles': vehicles})
 
 
 def detail(request, vehicle_id):
-    return None
+    vehicle = Vehicle.objects.get(pk=vehicle_id)
+    return render(request, 'vehicles/vehicle_details.html', {'vehicle': vehicle})
 
 
 def create(request):
