b3800413769afb7046718a065133f4d784786990
Filip Łapiński <f_lapinski@outlook.com>
2024-03-24 17:27:56 +0100

    Add logo in frontend and simple form to authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import busSyncLogo from './assets/logo.webp'
 import './App.css'
+function Logo() {
+	return (
+		<>
+			<div>
+				<img src={busSyncLogo} className='logo' alt='Bus-Sync logo' />
+			</div>
+		</>
+	)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Logo />
+			<div className='login'>
+				<label for=''>Login</label>
+				<input type='text'></input>
+				<br />
+				<label for=''>Password</label>
+				<input type='text'></input>
+			</div>
+		</>
+	)
 }
 
 export default App
