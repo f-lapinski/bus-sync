e7904fe360dfa0d926cdd41c853108a69f08cba8
f-lapinski <filip.lapinski@proton.me>
2024-03-25 11:16:26 +0100

    Split components to another files and css too

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,6 @@
-import { useState } from 'react'
-import busSyncLogo from './assets/logo.webp'
 import './App.css'
-function Logo() {
-	return (
-		<>
-			<div>
-				<img src={busSyncLogo} className='logo' alt='Bus-Sync logo' />
-			</div>
-		</>
-	)
-}
-
-export function LoginForm() {
-	return (
-		<>
-			<form action=''>
-				<div className='login-form'>
-					<div className='login-input'>
-						<input type='text' placeholder='Login'></input>
-					</div>
-					<div className='password-input'>
-						<input type='password' placeholder='Password'></input>
-					</div>
-					<div className='submit-button'>
-						<button type='submit'>Log in</button>
-					</div>
-				</div>
-				<p className='login-information'>You are don't have account? Contact with your administrator.</p>
-			</form>
-		</>
-	)
-}
+import Logo from './components/Logo/Logo.jsx'
+import LoginForm from './components/LoginForm/LoginForm.jsx'
 
 function App() {
 	return (
---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import { busSyncLogo } from '../../assets/logo.webp'
+import './Logo.css'
+
+export default function Logo() {
+	return (
+		<>
+			<div>
+				<img src={busSyncLogo} className='logo' alt='Bus-Sync logo' />
+			</div>
+		</>
+	)
+}
