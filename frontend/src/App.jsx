17ac9af844906cc25d68392cec4c4a9420d8fd93
f-lapinski <filip.lapinski@proton.me>
2024-03-25 09:10:26 +0100

    Styling login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,17 +11,32 @@ function Logo() {
 	)
 }
 
+export function LoginForm() {
+	return (
+		<>
+			<form action=''>
+				<div className='login-form'>
+					<div className='login-input'>
+						<input type='text' placeholder='Login'></input>
+					</div>
+					<div className='password-input'>
+						<input type='password' placeholder='Password'></input>
+					</div>
+					<div className='submit-button'>
+						<button type='submit'>Log in</button>
+					</div>
+				</div>
+				<p className='login-information'>You are don't have account? Contact with your administrator.</p>
+			</form>
+		</>
+	)
+}
+
 function App() {
 	return (
 		<>
 			<Logo />
-			<div className='login'>
-				<label for=''>Login</label>
-				<input type='text'></input>
-				<br />
-				<label for=''>Password</label>
-				<input type='text'></input>
-			</div>
+			<LoginForm />
 		</>
 	)
 }
