6d77775c988a9edb6ebdf79998008ee1b709bcc4
f-lapinski <filip.lapinski@proton.me>
2024-03-28 19:11:15 +0100

    Prepare Dashboard component

---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import './Dashboard.css'
+
+export default function Dashboard() {
+    return (
+        <>
+        <main>
+            <h5>Title</h5>
+            <p>Content</p>
+        </main>
+        </>
+    );
+}
